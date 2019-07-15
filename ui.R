d1bd438c8afd13f5a0afdbc5654b5e4e857b3967
Wonchan Kim <wonchankim97@gmail.com>
2019-07-15 13:33:47 -0400

    changed logos and fixed css script

--- ui.R
+++ ui.R
@@ -3,37 +3,52 @@ library(shinydashboard)
 library(tidyverse)
 
 shinyUI(
-  dashboardPage(
-    # change font
-    # tags$head(
-    #   tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
-    # ),
-    
-    
-    dashboardHeader(
-      title = "Suicide Rates",
-      titleWidth = 230,
-      tags$li('By Wonchan Kim',
-              style = 'text-align: right; padding-right: 5px; padding-top:17px; font-family: Arial, Helvetica, sans-serif;
-                            font-weight: bold;  font-size: 13px;',
-              class='dropdown'),
-      tags$li(a(href = 'https://github.com/wonchankim97',
-                img(src = 'GitHub_Logo.png',title = "github link", height = "18px")),
-              class = "dropdown")
+  fluidPage(
+    dashboardPage(
+      dashboardHeader(
+        title = "Suicide Rates",
+        titleWidth = 230,
+        tags$li('Wonchan Kim',
+                style = 'text-align: right; padding-right: 13px; padding-top:17px; font-family: Arial, Helvetica, sans-serif;
+                              font-weight: bold;  font-size: 13px;',
+                class='dropdown'),
+        tags$li(a(href = 'https://www.linkedin.com/in/wonchankim/',
+                  img(src = 'linkedin.png',title = "Wonchan's LinkedIn", height = "19px")),
+                class = "dropdown"),
+        tags$li(a(href = 'https://github.com/wonchankim97/Suicide-Rates',
+                  img(src = 'github.png',title = "Github Repository", height = "19px")),
+                class = "dropdown")
+        ),
+      dashboardSidebar(
+        sidebarUserPanel(
+          "Wonchan Kim",
+          image = "Me.png",
+          subtitle = 'NYC Data Science Fellow'
+        ),
+        sidebarMenu(
+          menuItem(text = 'Maps', icon = icon('globe'), tabName = 'maps'),
+          menuItem(text = 'Graphs', icon = icon('chart-bar'), tabName = 'graphs'),
+          menuItem(text = 'Machine Learning', icon = icon('laptop-code'), tabName = 'kmeans'),
+          menuItem(text = 'Data', icon = icon('database'), tabName = 'data')
+          # menuItem()
+        )
       ),
-    dashboardSidebar(
-      sidebarUserPanel(
-        "Wonchan Kim",
-        image = "Me.png"
-      ),
-      sidebarMenu(
-        menuItem(text = 'Maps', icon = icon('globe'), tabName = 'maps'),
-        menuItem(text = 'Graphs', icon = icon('chart-bar'), tabName = 'graphs'),
-        menuItem(text = 'Machine Learning', icon = icon('laptop-code'), tabName = 'kmeans'),
-        menuItem(text = 'Data', icon = icon('database'), tabName = 'data')
+      dashboardBody(
+        tags$head(
+          tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+        ),
+        
+        # optional element of allowing user to download data
+        # fluidRow(
+        #   column(12, downloadButton("downloadDataFromTable", "Download Table Data"))
+        # ),
+        # fluidRow(
+        #   column(12, DT::dataTableOutput("campaign_table", width = "100%"))
+        # )
+        
+        fluidRow()
       )
-    ),
-    dashboardBody()
+    )
   )
 )
 
